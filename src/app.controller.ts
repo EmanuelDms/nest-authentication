de710cc9307b65a509eb58e924b325b152d008fc
EmanuelDms <emanuel.duarte.maia@gmail.com>
2022-02-03 16:50:27 -0300

    feat: using env and remove login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,8 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
-import { LocalAuthGuard } from './common/guards/local.guard';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { EnvModule } from './env/env.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, EnvModule],
+  imports: [AuthModule, EnvModule],
   controllers: [AppController],
   providers: [AppService],
 })
